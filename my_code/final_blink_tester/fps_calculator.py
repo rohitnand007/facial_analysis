f2b922018fd09ca38b38354c1aafa323eb81b7bb
rohitnand007 <rohit.nand1007@gmail.com>
2019-07-16 15:30:44 -0600

    time elapsed from method

--- my_code/final_blink_tester/fps_calculator.py
+++ my_code/final_blink_tester/fps_calculator.py
@@ -4,7 +4,9 @@
 # python detect_blinks.py --shape-predictor shape_predictor_68_face_landmarks.dat
 
 # import the necessary packages
-from imutils.video import count_frames
+from imutils.video.count_frames import *
+from imutils.video import FPS
+import imutils
 import argparse
 import time
 import dlib
@@ -20,19 +22,31 @@ args = vars(ap.parse_args())
 
 # start the video stream thread
 print("[INFO] starting video stream thread...")
+fps1 = FPS().start()
 vs = cv2.VideoCapture(args["video"])
 fileStream = True
-# vs = VideoStream(src=0).start()
-# vs = VideoStream(args["video"]).start()
-# vs = VideoStream(usePiCamera=True).start()
-# fileStream = False
-time.sleep(1.0)
+vs.set(cv2.CAP_PROP_POS_AVI_RATIO,1)
+while True:
+	(grabbed,frame) = vs.read()
+	if grabbed:	
+		cv2.imshow("Frame", frame)
+		key = cv2.waitKey(1) & 0xFF
+		fps1.update()
+	else:
+		break	
+
 fps = vs.get(cv2.CAP_PROP_FPS)
 override = False if args["override"] < 0 else True
-total = count_frames(args["video"], override=override)
+total = 0#count_frames(args["video"], override=override)
+print("..........................................{}".format(fps))
+fps1.stop()
+elapsed = fps1.elapsed()
+time_elaspsed = vs.get(cv2.CAP_PROP_POS_MSEC)
 
 file1 = open("results.txt","a")
 str2 = "Frame rate for this video [{}] : Fps = [{}] & total_frames: [{}]/n.....".format(args["video"],fps,total)
 file1.write(str2)
 file1.close()
 print(str2) 
+print("Time elapsed is....:{}".format(elapsed))
+print("Time elaspsed from method is...................:{}".format(time_elaspsed))
