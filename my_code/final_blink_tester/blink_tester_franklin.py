4e7d935395e00270c930a75b393dc81772c79975
rohitnand007 <rohit.nand1007@gmail.com>
2019-07-17 13:16:00 -0600

    fps calculator tested for multiple seanrios

--- my_code/final_blink_tester/blink_tester_franklin.py
+++ my_code/final_blink_tester/blink_tester_franklin.py
@@ -3,9 +3,8 @@
 # python detect_blinks.py --shape-predictor shape_predictor_68_face_landmarks.dat
 
 # import the necessary packages
+from fps_calculator import fps_calculator
 from scipy.spatial import distance as dist
-from imutils.video import FileVideoStream
-from imutils.video import VideoStream
 from imutils import face_utils
 import numpy as np
 import argparse
@@ -14,3 +13,169 @@ import time
 import dlib
 import cv2
 import csv
+
+
+ 
+def eye_aspect_ratio(eye):
+	# compute the euclidean distances between the two sets of
+	# vertical eye landmarks (x, y)-coordinates
+	A = dist.euclidean(eye[1], eye[5])
+	B = dist.euclidean(eye[2], eye[4])
+
+	# compute the euclidean distance between the horizontal
+	# eye landmark (x, y)-coordinates
+	C = dist.euclidean(eye[0], eye[3])
+
+	# compute the eye aspect ratio
+	ear = (A + B) / (2.0 * C)
+
+	# return the eye aspect ratio
+	return ear
+ 
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--shape-predictor", required=True,
+	help="path to facial landmark predictor")
+ap.add_argument("-o", "--output", type = str, default = "outputy.mp4",
+	help="path to output video file")
+ap.add_argument("-f", "--fps", type=int, default=20,
+	help="FPS of output video")
+ap.add_argument("-c", "--codec", type=str, default="MJPG",
+	help="codec of output video")
+ap.add_argument("-v", "--video", required=True,
+    help="path to input video ")
+args = vars(ap.parse_args())
+ 
+# define two constants, one for the eye aspect ratio to indicate
+# blink and then a second constant for the number of consecutive
+# frames the eye must be below the threshold
+EYE_AR_THRESH = 0.3
+EYE_AR_CONSEC_FRAMES = 3
+
+# initialize the frame counters and the total number of blinks
+COUNTER = 0
+TOTAL = 0
+frame_counter = 0
+
+
+# initialize dlib's face detector (HOG-based) and then create
+# the facial landmark predictor
+print("[INFO] loading facial landmark predictor...")
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(args["shape_predictor"])
+
+# grab the indexes of the facial landmarks for the left and
+# right eye, respectively
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+(mStart, mEnd) = face_utils.FACIAL_LANDMARKS_IDXS["mouth"]
+
+# start the video stream thread
+print("[INFO] starting video stream thread...")
+vs = cv2.VideoCapture(args["video"]) #FileVideoStream(args["video"]).start()
+
+fourcc = cv2.VideoWriter_fourcc(*args["codec"])
+writer = None
+(h, w) = (None, None)
+zeros = None
+# loop over frames from the video stream
+try:
+	while True:
+	# if this is a file video stream, then we need to check if
+	# there any more frames left in the buffer to process
+		(grabbed,frame) = vs.read()
+		if grabbed:
+			# grab the frame from the threaded video file stream, resize
+			# it, and convert it to grayscale channels)
+
+			frame = imutils.resize(frame, width=450)
+			#frame = imutils.rotate_bound(frame, 90)
+			gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+			frame_counter += 1
+			#frame = imutils.rotate_bound(frame, 90)
+			# img_name = "junk_images/opencv_frame_{}.png".format(total_frame_counter)
+  			#cv2.imwrite(img_name, frame)
+			# detect faces in the grayscale frame
+			rects = detector(gray, 0)  	
+			# loop over the face detections
+			for rect in rects:
+				# determine the facial landmarks for the face region, then
+				# convert the facial landmark (x, y)-coordinates to a NumPy
+				# array
+				shape = predictor(gray, rect)
+				shape = face_utils.shape_to_np(shape)
+
+				# extract the left and right eye coordinates, then use the
+				# coordinates to compute the eye aspect ratio for both eyes
+				leftEye = shape[lStart:lEnd]
+				rightEye = shape[rStart:rEnd]
+				leftEAR = eye_aspect_ratio(leftEye)
+				rightEAR = eye_aspect_ratio(rightEye)
+
+				# average the eye aspect ratio together for both eyes
+				ear = (leftEAR + rightEAR) / 2.0	
+
+				# compute the convex hull for the left and right eye, then
+				# visualize each of the eyes
+				leftEyeHull = cv2.convexHull(leftEye)
+				rightEyeHull = cv2.convexHull(rightEye)
+				cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+				cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)	
+
+				# check to see if the eye aspect ratio is below the blink
+				# threshold, and if so, increment the blink frame counter
+				if ear < EYE_AR_THRESH:
+					COUNTER += 1
+				# otherwise, the eye aspect ratio is not below the blink
+				# threshold
+				else:
+					# if the eyes were closed for a sufficient number of
+					# then increment the total number of blinks
+					if COUNTER >= EYE_AR_CONSEC_FRAMES:
+						TOTAL += 1
+					# reset the eye frame counter
+					COUNTER = 0
+				# draw the total number of blinks on the frame along with
+				# the computed eye aspect ratio for the frame
+				cv2.putText(frame, "Blinks: {}".format(TOTAL), (10, 30),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+				cv2.putText(frame, "total_frames: {}".format(frame_counter),(10,60),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,0,0), 2)
+				cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)	
+				# write to frame
+	 			if writer is None:
+			 		(h, w) = frame.shape[:2]
+					writer = cv2.VideoWriter(args["output"], fourcc, args["fps"],
+					(w , h), True)
+					zeros = np.zeros((h, w), dtype="uint8")	
+				# show the frame
+				# cv2.imshow("Frame", frame)
+				#write it to a video file
+				writer.write(frame)
+				key = cv2.waitKey(1) & 0xFF
+
+				# if the `q` key was pressed, break from the loop
+				if key == ord("q"):
+					break
+				# do a bit of cleanup
+				writer.release()
+				cv2.destroyAllWindows()
+
+		else:
+			break
+
+except Exception as e:
+	raise e
+else:
+	pass
+finally:
+	csvData = []
+	csvData.append(["c38","c3","c2","c1","c0"])
+	csvData.append(["total_frames","total_smile_counter","Blinks_count"])
+
+	with open('eye_blinks.csv', 'wb') as csvFile:
+		writer = csv.writer(csvFile)
+		writer.writerows(csvData) 
+
+
--- my_code/final_blink_tester/fps_calculator.py
+++ my_code/final_blink_tester/fps_calculator.py
@@ -7,46 +7,57 @@
 from imutils.video.count_frames import *
 from imutils.video import FPS
 import imutils
-import argparse
 import time
 import dlib
 import cv2
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video", required=True,
-    help="path to input video ")
-ap.add_argument("-o", "--override", type=int, default=-1,
-	help="whether to force manual frame count")
-args = vars(ap.parse_args())
 
-# start the video stream thread
-print("[INFO] starting video stream thread...")
-fps1 = FPS().start()
-vs = cv2.VideoCapture(args["video"])
-fileStream = True
-vs.set(cv2.CAP_PROP_POS_AVI_RATIO,1)
-while True:
-	(grabbed,frame) = vs.read()
-	if grabbed:	
-		cv2.imshow("Frame", frame)
-		key = cv2.waitKey(1) & 0xFF
-		fps1.update()
-	else:
-		break	
+def fps_calculator(video):
+	fps = video.get(cv2.CAP_PROP_FPS)
+	override = False
+	if fps > 100:
+		override = True
+		frame_count = count_frames_manual(video)
+		video.set(cv2.CAP_PROP_POS_AVI_RATIO,1)
+		time_elaspsed = vs.get(cv2.CAP_PROP_POS_MSEC)/1000
+		fps = frame_count/float(time_elaspsed)
+	return fps	
 
-fps = vs.get(cv2.CAP_PROP_FPS)
-override = False if args["override"] < 0 else True
-total = count_frames_manual(vs)#count_frames(args["video"], override=override)
-print("..........................................{}".format(fps))
-fps1.stop()
-elapsed = fps1.elapsed()
-time_elaspsed = vs.get(cv2.CAP_PROP_POS_MSEC)
+# # construct the argument parse and parse the arguments
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-v", "--video", required=True,
+#     help="path to input video ")
+# ap.add_argument("-o", "--override", type=int, default=-1,
+# 	help="whether to force manual frame count")
+# args = vars(ap.parse_args())
 
-file1 = open("results.txt","a")
-str2 = "Frame rate for this video [{}] : Fps = [{}] & total_frames: [{}]/n.....".format(args["video"],fps,total)
-file1.write(str2)
-file1.close()
-print(str2) 
-print("Time elapsed is....:{}".format(elapsed))
-print("Time elaspsed from method is...................:{}".format(time_elaspsed))
+# # start the video stream thread
+# print("[INFO] starting video stream thread...")
+# fps1 = FPS().start()
+# vs = cv2.VideoCapture(args["video"])
+# fileStream = True
+# vs.set(cv2.CAP_PROP_POS_AVI_RATIO,1)
+# while True:
+# 	(grabbed,frame) = vs.read()
+# 	if grabbed:	
+# 		cv2.imshow("Frame", frame)
+# 		key = cv2.waitKey(1) & 0xFF
+# 		fps1.update()
+# 	else:
+# 		break	
+
+# fps = vs.get(cv2.CAP_PROP_FPS)
+# override = False if args["override"] < 0 else True
+# total = count_frames_manual(vs)#count_frames(args["video"], override=override)
+# print("..........................................{}".format(fps))
+# fps1.stop()
+# elapsed = fps1.elapsed()
+# time_elaspsed = vs.get(cv2.CAP_PROP_POS_MSEC)
+
+# file1 = open("results.txt","a")
+# str2 = "Frame rate for this video [{}] : Fps = [{}] & total_frames: [{}]/n.....".format(args["video"],fps,total)
+# file1.write(str2)
+# file1.close()
+# print(str2) 
+# print("Time elapsed is....:{}".format(elapsed))
+# print("Time elaspsed from method is...................:{}".format(time_elaspsed))
