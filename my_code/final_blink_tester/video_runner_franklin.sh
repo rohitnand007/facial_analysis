bfe83dce0f8ddb4f22ca1f8380ec4e9a43152e80
rohityerramsetty <rohit.nand1007@gmail.com>
2019-07-31 15:17:06 -0600

    video runner--bug fixes

--- my_code/final_blink_tester/video_runner_franklin.sh
+++ my_code/final_blink_tester/video_runner_franklin.sh
@@ -1,21 +1,22 @@
 # ./video_runner.sh "${ARR[@]}"
 ARR=( "$@" )
-
+videos=()
 for i in "${ARR[@]}"
 do
 	:
 	#videos=$(find /na/homes/ryerramsetty/../../../export/research/analysis/human/kkiehl/media/ -name "*$i**.wmv")
 	# printf "ARR array contains %d elements: " ${#videos[@]}
 	# printf ' ->%s\n' "${videos[@]}"
-	videos=()
 	while IFS=  read -r -d $'\0'; do
-    videos+=("$REPLY")
+		videos+=("$REPLY")
 	done < <(find /na/homes/ryerramsetty/../../../export/research/analysis/human/kkiehl/media/ -name "*$i**.wmv" -print0)
-	printf "ARR array contains %d elements: " ${#videos[@]}
-	printf ' ->%s\n' "${videos[@]}"
-	# for j in "${videos[@]}"
-	# do
-	# 	:
-	# 	python blink_tester_franklin.py --shape-predictor ~/file_transfer_bucket/shape_predictor_68_face_landmarks.dat --video $j
-	# done
-done	
+	
+	for j in "${videos[@]}"
+	do
+	 	:
+	 	python blink_tester_franklin.py --shape-predictor ~/file_transfer_bucket/shape_predictor_68_face_landmarks.dat --video $j
+	 done
+done
+printf ' ->%s\n' "${videos[@]}"	
+printf "ARR array contains %d elements: " ${#videos[@]}
+
