7a361eccc844928b7ea1e4f69ceeb224564e8bed
rohitnand007 <rohit.nand1007@gmail.com>
2019-07-31 13:36:12 -0600

    video_runner shell script to run files for blink testing added

---
+++ my_code/final_blink_tester/video_runner_franklin.sh
@@ -0,0 +1,15 @@
+# ./video_runner.sh "${ARR[@]}"
+ARR=( "$@" )
+
+for i in "${ARR[@]}"
+do
+	:
+	videos=$(find /na/homes/ryerramsetty/../../../export/research/analysis/human/kkiehl/media/ -name "*$i**.wmv")
+	printf "ARR array contains %d elements: " ${#videos[@]}
+	printf ' ->%s\n' "${videos[@]}"
+	for j in "${videos[@]}"
+	do
+		:
+		python blink_tester_franklin.py --shape-predictor ~/file_transfer_bucket/shape_predictor_68_face_landmarks.dat --video $j
+	done
+done	
