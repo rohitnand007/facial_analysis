882c5545331979d99af78f837e85818b49051eb7
rohitnand007 <rohit.nand1007@gmail.com>
2019-09-05 11:49:12 -0600

    bash_script test folder update

--- dev_code/emotion_segregation/bash_script_test.sh
+++ dev_code/emotion_segregation/bash_script_test.sh
@@ -9,12 +9,12 @@ do
 	# printf ' ->%s\n' "${videos[@]}"
 	while IFS=  read -r -d $'\0'; do
 		videos+=("$REPLY")
-	done < <(find /na/homes/ryerramsetty/../../../export/research/analysis/human/kkiehl/media/ -name "*$i**.wmv" -print0)
+	done < <(find /na/homes/ryerramsetty/../../../export/research/analysis/human/kkiehl/media/adult_06408/dropoff/ -name "*$i**.wmv" -print0)
 	
 	for j in "${videos[@]}"
 	do
 	 	:
-	 	printf "this video runs here: " ${#j}
+	 	printf "this video runs here: "
 	 done
 done
 printf ' ->%s\n' "${videos[@]}"	
