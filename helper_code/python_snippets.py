7d2831929923cd8434a448f30d0e7e2e898af56e
rohitnand007 <rohit.nand1007@gmail.com>
2019-09-24 12:46:25 -0600

    helper_snippets code updated with 2 methods

---
+++ helper_code/python_snippets.py
@@ -0,0 +1,17 @@
+# code snippet for searching for certain kind of videos
+import os,glob
+path = os.path.expanduser("~") + "/../../../export/research/analysis/human/kkiehl/media/"
+addl_path = "BBP_20150/Assessment_Videos/NewMexico/Adult_Incarcerated/Male"
+
+videos_array = glob.glob(path + addl_path +"/"+"**PCLR**.wmv")
+
+
+#to make a python array to bash array
+
+def bash_array(python_array):
+	ini_string = ""
+	for title in python_array:
+		ini_string += '\"{}\" '.format(title)
+	return a.strip()
+
+		
