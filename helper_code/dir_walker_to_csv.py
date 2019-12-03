ef0570ec80c98d7bd6d477862d789a089be3ed50
rohitnand007 <rohit.nand1007@gmail.com>
2019-12-03 03:57:09 -0700

    os walk to csv file converter

---
+++ helper_code/dir_walker_to_csv.py
@@ -0,0 +1,33 @@
+import os
+import argparse
+import csv
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--folder", required=True,
+	help="path to input folder of videos")
+args = vars(ap.parse_args())
+
+files_folder = args["folder"]
+
+files_list = []
+
+def get_files_list(files_folder):
+
+	for root, dirs, files in os.walk(files_folder):
+			for f in files:
+				if (f.endswith('.wmv') or f.endswith('.mp4')): 
+					files_list.append(root + f)
+	return files_list
+
+a = get_files_list(files_folder)
+
+file_chunks = [a[x:x+50] for x in xrange(0, len(a), 50)]
+
+for i,chunk in enumerate(file_chunks):
+	output_filename = 'output_csvs/pose_list_' + str(i) + ".csv" 
+	with open(output_filename, 'w') as file:
+		writer = csv.writer(file)
+		for name in chunk:	
+			writer.writerow([name])
+
+						
